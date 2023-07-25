ac3cdcdc2f96a398405d9193cae1ba94c948981e
Gerard <kanetpwnz@gmail.com>
2023-07-25 23:10:57 +0200

    fixed bugs

--- mastermind.rb
+++ mastermind.rb
@@ -1,9 +1,12 @@
 class Mastermind
-  attr_accessor :colores_dispo, :clave_length
+  attr_accessor :colores_dispo, :clave_length, :resultado, :solucion, :guess
 
   def initialize
     @colores_dispo = ['R', 'A', 'N', 'V', 'T', 'M']
     @clave_length = 4
+    @resultado = ''
+    @guess = ''
+    @solucion = []
     @breaker = Breaker.new(self)
     @maker = Maker.new(self)
   end
@@ -13,13 +16,26 @@ class Mastermind
       puts 'Si quieres adivinar introduce una A, si quieres que te adivinen introduce una M (a/m):'
       awnser = gets.chomp.downcase
       if awnser == 'a'
-        puts 'Empieza el juego:'
+        puts 'Empieza el juego, tú adivinas!:'
         @breaker.turnos_breaker
       elsif awnser == 'm'
-        puts 'Empieza el juego:'
+        puts 'Empieza el juego, la máquina te adivina!:'
         @maker.clave_human
       end
     end
+  end
+
+  def game_rules
+    @resultado = ''
+    for i in 0..(@clave_length - 1)
+      if @solucion[i] == @guess[i]
+        @resultado += 'O'
+      elsif @solucion[i] == @guess[0] || @solucion[i] == @guess[1] || @solucion[i] == @guess[2] || @solucion[i] == @guess[3]
+        @resultado += 'X'
+      end
+    end
+    puts 'Si la posición es correcta, se mostrará "O", si existe pero no está en orden, será "X":'
+    puts @resultado
   end
 
   def game_over
@@ -36,23 +52,21 @@ class Mastermind
 end
 
 class Maker
-  attr_accessor :solucion, :resultado
+  attr_accessor
 
   def initialize(mastermind)
     @mastermind = mastermind
-    @solucion = []
-    @resultado = ''
   end
 
   def clave_human
     loop do
       puts 'Introduzca su clave:'
-      @solucion = gets.chomp.upcase.split('')
-      if @solucion.length != @mastermind.clave_length
+      @mastermind.solucion = gets.chomp.upcase.split('')
+      if @mastermind.solucion.length != @mastermind.clave_length
         puts "Su clave debe tener #{@mastermind.clave_length} caracteres."
         next
       end
-      if !@solucion.index{ |x| !@mastermind.colores_dispo.include?(x) }.nil?
+      if !@mastermind.solucion.index{ |x| !@mastermind.colores_dispo.include?(x) }.nil?
         puts "Su clave debe contener los siguientes colores: #{@mastermind.colores_dispo}."
         next
       end
@@ -60,44 +74,55 @@ class Maker
     end
   end
 
-# 12 turnos + seguir normas, no bruteforce
   def computer_rules
+    for i in 1..12
+      if @mastermind.solucion == @mastermind.guess
+        puts 'Lá máquina ha descifrado su clave, GAME OVER!!!'
+        @mastermind.game_over
+      end
+      puts "Intento #{i} de 12."
+      computer_guess
+      @mastermind.game_rules
+    end
+    puts 'Se acabaron los intentos de la máquina, TÚ GANAS!!!'
+    @mastermind.game_over
+  end
+
+  def computer_guess
+    puts "La máquina prueba con: #{@mastermind.guess}."
   end
 end
 
 class Breaker
-  attr_accessor :solucion, :resultado
+  attr_accessor
 
   def initialize(mastermind)
     @mastermind = mastermind
-    @solucion = []
-    @resultado = ''
   end
 
   def computer_choice
     for i in 1..@mastermind.clave_length
-      @solucion.push(@mastermind.colores_dispo.sample)
+      @mastermind.solucion.push(@mastermind.colores_dispo.sample)
     end
-    # recordar borrar este puts para q no se vea la solución
-    #
-    puts @solucion
+    puts @mastermind.solucion
+    puts 'La máquina ya a pensado en su combinación! Ahora adivinala!'
   end
 
   def user_guess
     loop do
-      puts 'Introduzca su conjetura:'
-      @guess = gets.chomp.upcase.split('')
-      if @guess.length != @mastermind.clave_length
+      puts 'Introduzca su conjetura, colores disponibles: Rojo, Amarillo, Negro, Verde, Turquesa, Marrón:'
+      @mastermind.guess = gets.chomp.upcase.split('')
+      if @mastermind.guess.length != @mastermind.clave_length
         puts "Su conjetura debe tener #{@mastermind.clave_length} caracteres."
         next
       end
-      if !@guess.index{ |x| !@mastermind.colores_dispo.include?(x) }.nil?
+      if !@mastermind.guess.index{ |x| !@mastermind.colores_dispo.include?(x) }.nil?
         puts "Su conjetura debe contener los siguientes colores: #{@mastermind.colores_dispo}."
         next
       end
-      if @solucion == @guess
+      if @mastermind.solucion == @mastermind.guess
         puts 'Tenemos un ganador!!!'
-        exit
+        @mastermind.game_over
       end
       return
     end
@@ -108,23 +133,10 @@ class Breaker
     for i in 1..12
       puts "Intento #{i} de 12."
       user_guess
-      game_rules
+      @mastermind.game_rules
     end
-    puts 'Se acabaron tus intentos, GAME OVER!'
+    puts 'Se acabaron tus intentos, GAME OVER!!!'
     @mastermind.game_over
-  end
-
-  def game_rules
-    @resultado = ''
-    for i in 0..(@mastermind.clave_length - 1)
-      if @solucion[i] == @guess[i]
-        @resultado += 'O'
-      elsif @solucion[i] == @guess[0] || @solucion[i] == @guess[1] || @solucion[i] == @guess[2] || @solucion[i] == @guess[3]
-        @resultado += 'X'
-      end
-    end
-    puts 'Si la posición es correcta, se mostrará "O", si existe pero no está en orden, será "X":'
-    puts @resultado
   end
 end
 
