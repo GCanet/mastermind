1292135e1409079c434c08078f67998abbff7e0e
Gerard <kanetpwnz@gmail.com>
2023-07-25 14:16:17 +0200

    working product

--- mastermind.rb
+++ mastermind.rb
@@ -1,26 +1,30 @@
 class Mastermind
-  attr_accessor :awnser, :awnser2
+  attr_accessor :colores_dispo, :clave_length
 
-  def initialize(breaker, maker)
-    @breaker = breaker
-    @maker = maker
+  def initialize
+    @colores_dispo = ['R', 'A', 'N', 'V', 'T', 'M']
+    @clave_length = 4
+    @breaker = Breaker.new(self)
+    @maker = Maker.new(self)
   end
 
   def playinicial
     loop do
-      puts 'Si quieres adivinar introduce una A, si quieres que te adivinen introduce una M'
+      puts 'Si quieres adivinar introduce una A, si quieres que te adivinen introduce una M (a/m):'
       awnser = gets.chomp.downcase
       if awnser == 'a'
-        Breaker.turnos_breaker
+        puts 'Empieza el juego:'
+        @breaker.turnos_breaker
       elsif awnser == 'm'
-        Maker.clave_human
+        puts 'Empieza el juego:'
+        @maker.clave_human
       end
     end
   end
 
   def game_over
     loop do
-      puts 'Volver a jugar? y/n'
+      puts 'Volver a jugar? (y/n):'
       awnser2 = gets.chomp.downcase
       if awnser2 == 'y'
         playinicial
@@ -32,24 +36,24 @@ class Mastermind
 end
 
 class Maker
-  attr_accessor :solucion_maker, :resultado_maker
+  attr_accessor :solucion, :resultado
 
   def initialize(mastermind)
     @mastermind = mastermind
-    @solucion_maker = []
-    @resultado_maker = ''
+    @solucion = []
+    @resultado = ''
   end
 
   def clave_human
     loop do
       puts 'Introduzca su clave:'
-      @solucion_maker = gets.chomp.upcase.split('')
-      if @solucion_maker.length != @clave_length
-        puts "Su clave debe tener #{@clave_length} caracteres."
+      @solucion = gets.chomp.upcase.split('')
+      if @solucion.length != @mastermind.clave_length
+        puts "Su clave debe tener #{@mastermind.clave_length} caracteres."
         next
       end
-      if !@solucion_maker.index{ |x| !@colores_dispo.include?(x) }.nil?
-        puts "Su clave debe contener los siguientes colores: #{@colores_dispo}."
+      if !@solucion.index{ |x| !@mastermind.colores_dispo.include?(x) }.nil?
+        puts "Su clave debe contener los siguientes colores: #{@mastermind.colores_dispo}."
         next
       end
       return
@@ -62,19 +66,17 @@ class Maker
 end
 
 class Breaker
-  attr_accessor :solucion, :clave_length, :colores_dispo, :resultado
+  attr_accessor :solucion, :resultado
 
-  def initialize (mastermind)
-    @colores_dispo = ['R', 'A', 'N', 'V', 'T', 'M']
-    @clave_length = 4
+  def initialize(mastermind)
+    @mastermind = mastermind
     @solucion = []
     @resultado = ''
-    @mastermind = mastermind
   end
 
   def computer_choice
-    for i in 1..@clave_length
-      @solucion.push(@colores_dispo.sample)
+    for i in 1..@mastermind.clave_length
+      @solucion.push(@mastermind.colores_dispo.sample)
     end
     # recordar borrar este puts para q no se vea la solución
     #
@@ -85,12 +87,12 @@ class Breaker
     loop do
       puts 'Introduzca su conjetura:'
       @guess = gets.chomp.upcase.split('')
-      if @guess.length != @clave_length
-        puts "Su conjetura debe tener #{@clave_length} caracteres."
+      if @guess.length != @mastermind.clave_length
+        puts "Su conjetura debe tener #{@mastermind.clave_length} caracteres."
         next
       end
-      if !@guess.index{ |x| !@colores_dispo.include?(x) }.nil?
-        puts "Su conjetura debe contener los siguientes colores: #{@colores_dispo}."
+      if !@guess.index{ |x| !@mastermind.colores_dispo.include?(x) }.nil?
+        puts "Su conjetura debe contener los siguientes colores: #{@mastermind.colores_dispo}."
         next
       end
       if @solucion == @guess
@@ -109,11 +111,12 @@ class Breaker
       game_rules
     end
     puts 'Se acabaron tus intentos, GAME OVER!'
-    Mastermind.game_over
+    @mastermind.game_over
   end
 
   def game_rules
-    for i in 0..(@clave_length - 1)
+    @resultado = ''
+    for i in 0..(@mastermind.clave_length - 1)
       if @solucion[i] == @guess[i]
         @resultado += 'O'
       elsif @solucion[i] == @guess[0] || @solucion[i] == @guess[1] || @solucion[i] == @guess[2] || @solucion[i] == @guess[3]
